c3f119f8ea8324719ec95263ee886a8721773c75
troworld <troworld@e96f18ea-5749-0410-b079-a370ac824929>
2008-06-03 23:25:44 +0000

    Forgot to check in pjsserver.py
    git-svn-id: http://pjabberd.googlecode.com/svn/trunk@71 e96f18ea-5749-0410-b079-a370ac824929

---
+++ pjs/pjsserver.py
@@ -0,0 +1,108 @@
+import pjs.conf.conf
+import logging
+import os, os.path, sys
+
+from pjs.db import DB, sqlite
+from pjs.router import Router
+
+class PJSLauncher:
+    """The one and only instance of the server. This controls all other
+    components.
+    """
+    def __init__(self):
+        self.servers = []
+        
+        self.c2sport = 5222
+        self.s2sport = 5269
+        
+        self.hostname = 'localhost'
+        
+        self.router = Router(self)
+        
+    def run(self):
+        from pjs.server import C2SServer, S2SServer
+        self.servers.append(C2SServer(self.hostname, self.c2sport, self))
+        
+        s2s = S2SServer(self.hostname, self.s2sport, self)
+        self.router.setConnMap(s2s.s2sConns)
+        
+        self.servers.append(s2s)
+
+if __name__ == '__main__':
+    launcher = PJSLauncher()
+    pjs.conf.conf.launcher = launcher
+    
+    # TODO: move all of this into a config file + parser
+    
+    logFileName = 'server-log'
+    logDir = 'log'
+    logLoc = os.path.join(logDir, logFileName)
+    logLevel = logging.DEBUG
+    
+    def configLogging(filename=logFileName, level=logLevel,
+                     format='%(asctime)s %(levelname)-8s %(message)s'):
+        try:
+            logging.basicConfig(filename=filename, level=level, format=format)
+        except IOError:
+            print >> sys.stderr, 'Could not create a log file. Logging to stderr.'
+            logging.basicConfig(level=level, format=format)
+    
+    if os.path.exists('log'):
+        if os.path.isdir('log') and os.access('log', os.W_OK):
+            configLogging(logLoc)
+        else:
+            print >> sys.stderr, 'Logging directory is not accessible'
+            configLogging()
+    else:
+        try:
+            os.mkdir('log')
+            configLogging(logLoc)
+        except IOError:
+            print >> sys.stderr, 'Could not create logging directory'
+            configLogging()
+        
+    con = DB()
+    c = con.cursor()
+    try:
+        c.execute("CREATE TABLE jids (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
+                                        jid TEXT NOT NULL,\
+                                        password TEXT NOT NULL,\
+                                        UNIQUE(jid))")
+        c.execute("CREATE TABLE roster (userid INTEGER REFERENCES jids NOT NULL,\
+                                        contactid INTEGER REFERENCES jids NOT NULL,\
+                                        name TEXT,\
+                                        subscription INTEGER DEFAULT 0,\
+                                        PRIMARY KEY (userid, contactid)\
+                                        )")
+        c.execute("CREATE TABLE rostergroups (groupid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
+                                              userid INTEGER REFERENCES jids NOT NULL,\
+                                              name TEXT NOT NULL,\
+                                              UNIQUE(userid, name)\
+                                              )")
+        c.execute("CREATE TABLE rostergroupitems\
+                    (groupid INTEGER REFERENCES rostergroup NOT NULL,\
+                     contactid INTEGER REFERENCES jids NOT NULL,\
+                     PRIMARY KEY (groupid, contactid))")
+        c.execute("INSERT INTO jids (jid, password) VALUES ('tro@localhost', 'test')")
+        c.execute("INSERT INTO jids (jid, password) VALUES ('dv@localhost', 'test')")
+        c.execute("INSERT INTO roster (userid, contactid, subscription) VALUES (1, 2, 8)")
+        c.execute("INSERT INTO roster (userid, contactid, subscription) VALUES (2, 1, 8)")
+        c.execute("INSERT INTO rostergroups (userid, name) VALUES (1, 'friends')")
+        c.execute("INSERT INTO rostergroups (userid, name) VALUES (1, 'weirdos')")
+        c.execute("INSERT INTO rostergroupitems (groupid, contactid) VALUES (1, 2)")
+    except sqlite.OperationalError, e:
+        if e.message.find('already exists') >= 0: pass
+        else: raise
+    c.close()
+    
+    launcher.run()
+    logging.info('server started')
+    
+    import pjs.async.core
+    
+    try:
+        pjs.async.core.loop()
+    except KeyboardInterrupt:
+        # clean up
+        logging.info("KeyboardInterrupt sent. Shutting down...")
+        logging.shutdown()
