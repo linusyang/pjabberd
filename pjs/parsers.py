51e85807bf551ca876f782d7b02ba25e31256ba6
troworld <troworld@e96f18ea-5749-0410-b079-a370ac824929>
2008-06-03 23:11:31 +0000

    Some basic parsing code based on David's session.py. The parser creates an ElementTree for each stanza and keeps around a reference to the stream, so that we can attach the stanza to it and pass it to the handlers. + some tests
    git-svn-id: http://pjabberd.googlecode.com/svn/trunk@10 e96f18ea-5749-0410-b079-a370ac824929

--- pjs/parsers.py
+++ pjs/parsers.py
@@ -1,6 +1,7 @@
 """ XML Stream parsers """
 
 from xml.parsers import expat
+import pjs.elementtree.ElementTree as et
 
 class IncrStreamParser:
     """Pass it unicode strings via feed() and it will buffer the input until it
@@ -11,10 +12,28 @@ class IncrStreamParser:
 
     def __init__(self, conn=None):
         self.conn = conn
-        self._parser = expat.ParserCreate()
+        # '}' is a ns-separator used in ET 1.3alpha. We want to duplicate its
+        # behaviour here because its TreeBuilder doesn't prefix node names
+        # with their namespace. Asking expat to do so will remove the xmlns
+        # attrs from elements it encounters.
+        self._parser = expat.ParserCreate(None, '}')
         self._parser.StartElementHandler = self.handle_start
         self._parser.EndElementHandler = self.handle_end
         self._parser.CharacterDataHandler = self.handle_text
+        self._parser.buffer_text = 1 # single handle_text call per text node
+        
+        self._names = {} # name memo cache. from ElementTree
+        
+        # this is the main <stream> et.Element
+        self.stream = None
+        
+        self.reset()
+    
+    def reset(self):
+        """Reset the stream"""
+        self.depth = 0
+        self.tree = None
+        self.stream = None
 
     def feed(self, data):
         """Read a chunk of data to parse. The complete XML in the chunk will
@@ -27,15 +46,82 @@ class IncrStreamParser:
         """CLose the stream of XML data"""
         self._parser.Parse("", 1) # end of data
         del self._parser # get rid of circular references
+        
+        self.reset()
 
     def handle_start(self, tag, attrs):
+        """Handles the opening-tag event. It is fired whenever the closing
+        bracket of an opening XML element is encountered (ie. '>' in "<stream>").
+        """
         # some logic to bootstrap a handling phase
         # include logic for starting streams
         # build an ElementTree for all stanzas
-        pass
+        
+        self.depth += 1
+        
+        assert(self.depth >= 1)
+        
+        if self.depth == 1:
+            # handle <stream>, record it for XPath wrapping
+            
+            # if starting a new stream, reset the old one
+            if self.stream: self.reset()
+            self.stream = et.Element(self._fixname(tag), attrs)
+            pass
+        elif self.depth == 2:
+            # handle stanzas, build tree
+            self.tree = et.TreeBuilder()
+            self.tree.start(self._fixname(tag), attrs)
+        else:
+            # depth > 2. continue to build tree
+            assert(self.tree)
+            self.tree.start(self._fixname(tag), attrs)
 
     def handle_end(self, tag):
-        pass
+        """Handles the closing-tag event. It is fired whenever the closing
+        bracket of a closing XML element is encountered (ie. '>' in "</stream>").
+        """
+        self.depth -= 1
+        
+        assert(self.depth >= 0)
+        
+        if self.depth == 0:
+            # handle </stream>. don't reset because we may need the data
+            pass
+        elif self.depth == 1:
+            # handle </stanza>
+            assert(self.tree)
+            self.tree.end(self._fixname(tag))
+            # TODO: handle errors
+            self.tree = self.tree.close()
+            # TODO: pass the el to the dispatcher for processing
+        else:
+            # depth > 1. continue to build tree
+            assert(self.tree)
+            self.tree.end(self._fixname(tag))
 
     def handle_text(self, text):
-        pass
+        """Handles the text node event."""
+        # TODO: test on very large text node. Will expat's buffer overflow?
+        
+        assert(self.tree)
+        
+        if self.depth <= 1:
+            # TODO: there can't be any text between stanzas/streams, so maybe close the stream?
+            pass
+        else:
+            self.tree.data(text)
+            
+    def _fixname(self, key):
+        """Formats the node name according to ElementTree's convention of
+        {ns}tag.
+        """
+        # expand qname. from ElementTree.py
+        try:
+            name = self._names[key]
+        except KeyError:
+            name = key
+            if "}" in name:
+                name = "{" + name
+            self._names[key] = name
+        return name
--- pjs/test/test_parsers.py
+++ pjs/test/test_parsers.py
@@ -1,8 +1,31 @@
 import unittest
+import copy
 import xml.parsers.expat
 from pjs.parsers import IncrStreamParser
 
-class TestParsers(unittest.TestCase):
+streamStart = """<stream:stream xmlns='jabber:client' \
+            xmlns:stream='http://etherx.jabber.org/streams' \
+            id='c2s_345' from='example.com' version='1.0'>"""
+streamEnd = '</stream:stream>'
+
+class DumbParser(IncrStreamParser):
+    """We don't want to handle anything, just parse.
+    Still, we record when the start/end/text events have occurred.
+    """
+    def __init__(self):
+        IncrStreamParser.__init__(self)
+        self.startCalled = False
+        self.endCalled = False
+        self.textCalled = False
+    def handle_start(self, tag, attrs):
+        self.startCalled = True
+    def handle_end(self, tag):
+        self.endCalled = True
+    def handle_text(self, text):
+        self.textCalled = True
+
+class TestDumbParser(unittest.TestCase):
+    """Only test the partial XML parsing, not its handling"""
     
     def setUp(self):
         unittest.TestCase.setUp(self)
@@ -47,21 +70,94 @@ class TestParsers(unittest.TestCase):
         self.p.feed('<stream:stream')
         self.failUnlessRaises(xml.parsers.expat.ExpatError, self.p.close)
         
-class DumbParser(IncrStreamParser):
-    """We don't want to handle anything, just parse.
-    Still, we record when the start/end/text events have occurred.
-    """
-    def __init__(self):
-        IncrStreamParser.__init__(self)
-        self.startCalled = False
-        self.endCalled = False
-        self.textCalled = False
-    def handle_start(self, tag, attrs):
-        self.startCalled = True
-    def handle_end(self, tag):
-        self.endCalled = True
-    def handle_text(self, text):
-        self.textCalled = True
+class TestParser(unittest.TestCase):
+    """Test tree-building of the incremental parser"""
+    
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.p = IncrStreamParser()
+        
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+        
+        try:
+            self.p.close() 
+        except xml.parsers.expat.ExpatError:
+            pass
+        
+    def testOpenStream(self):
+        """Depth should increase at stream element"""
+        self.p.feed(streamStart)
+        self.assert_(self.p.depth == 1)
+        self.assert_(self.p.tree is None)
+        
+    def testOpenCloseStream(self):
+        """Testing depth and initialization of tree"""
+        self.p.feed(streamStart)
+        self.p.feed(streamEnd)
+        self.assert_(self.p.depth == 0)
+        self.assert_(self.p.tree is None)
+        
+    def testKeepStream(self):
+        """Should keep the <stream> Element even when we've received </stream>.
+        This is so that any threaded handlers can use it.
+        """
+        self.p.feed(streamStart)
+        self.p.feed(streamEnd)
+        
+        self.assert_(self.p.stream is not None)
+        
+    def testSimpleStanza(self):
+        """Single-level empty stanza"""
+        self.p.feed(streamStart)
+        
+        self.p.feed('<stream:features>')
+        self.assert_(self.p.depth == 2)
+        self.assert_(self.p.tree is not None)
+        
+        self.p.feed('</stream:features>')
+        
+        # copying for our own use
+        self.assert_(self.p.tree.tag == '{http://etherx.jabber.org/streams}features')
+        self.assert_(self.p.depth == 1)
+        
+    def testSimpleStanzaKeepTree(self):
+        """Single-level empty stanza. Should keep self.p.tree after stanza is
+        fully parsed, since we may refer to the tree after the closing element
+        is handled.
+        """
+        self.p.feed(streamStart)
+        self.p.feed('<stream:features>')
+        self.p.feed('</stream:features>')
+        self.p.feed(streamEnd)
+        
+        self.assert_(self.p.tree is not None)
+        
+    def testDeepStanza(self):
+        """Multi-level stanza"""
+        self.p.feed(streamStart)
+        
+        self.p.feed("""<iq type='result' id='bind_2'>\
+                      <bind xmlns='urn:ietf:params:xml:ns:xmpp-bind'>\
+                        <jid>somenode@example.com/someresource""")
+        self.assert_(self.p.depth == 4)
+        
+        self.p.feed("</jid></bind></iq>")
+        self.assert_(self.p.depth == 1)
+        self.assert_(self.p.tree.tag == '{jabber:client}iq')
+        self.assert_(self.p.tree[0].tag == '{urn:ietf:params:xml:ns:xmpp-bind}bind')
+        self.assert_(self.p.tree[0][0].text == u'somenode@example.com/someresource')
+
+    def testNamespacedXPath(self):
+        """Should be able to refer to elements using namespaces in XPath"""
+        self.p.feed(streamStart)
+        self.p.feed('<stream:features></stream:features>')
+        self.p.feed(streamEnd)
+        
+        stream = copy.copy(self.p.stream)
+        stream.insert(0, self.p.tree)
+        
+        self.assert_(stream.find('{http://etherx.jabber.org/streams}features') is not None)
         
 if __name__ == '__main__':
     unittest.main()
